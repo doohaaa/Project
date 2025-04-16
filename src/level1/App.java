d67f7405c628a0d57fae9f1ea4dcba66d3eae09f
doohaaa <engk5423@naver.com>
2025-04-16 19:51:32 +0900

    예외처리 없애고 전체 구조 수정

--- src/level1/App.java
+++ src/level1/App.java
@@ -1,5 +1,6 @@
 package level1;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class App {
@@ -8,57 +9,27 @@ public class App {
         // 변수 생성 및 초기화
 
         Scanner scanner = new Scanner(System.in);
-        int num1 = 0;
-        int num2 = 0;
+
         int result = 0;
-        String str = "연산기호";
-        boolean flag = true;
-        boolean flag2 = true;
+        String exit ;
 
         // 1. 양의 정수를 입력받기
-
-
-        while (flag) {
+        // 2. 사칙연산 기호 입력받기
+        do {
             System.out.print("첫번째 숫자를 입력하세요: ");
-            num1 = scanner.nextInt();
-            if (num1 >= 0) {
-                flag = false;
-            } else {
+            int num1 = scanner.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String str = scanner.next();
+
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+            if (num2 == 0) {
                 System.out.println("오류 : 0보다 크거나 같은 정수를 입력하세요.");
             }
-        }
 
-        flag = true;
-        while (flag2) {
-            while (flag) {
-                {
-                    System.out.print("두번째 숫자를 입력하세요: ");
-                    num2 = scanner.nextInt();
-                    if (num2 >= 0) {
-                        flag = false;
-                    } else {
-                        System.out.println("오류 : 0보다 크거나 같은 정수를 입력하세요.");
-                    }
-                }
-            }
 
-            // 2. 사칙연산 기호
-            flag = true;
-
-            while (flag) {
-                {
-                    System.out.print("사칙연산 기호를 입력하세요: ");
-                    str = scanner.next();
-
-                    if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
-                        flag = false;
-                    } else {
-                        System.out.println("오류 : 올바른 기호를 입력하세요.");
-                    }
-                }
-            }
-            flag = true;
-            // 3. 사칙연산 기호를 사용하여 연산을 진행 한 후 결과를 출력하세요.
+            // 3. 사칙연산 기호를 사용하여 연산을 진행 한 후 결과를 출력하기
 
             if (str.equals("+")) {
                 result = num1 + num2;
@@ -66,17 +37,24 @@ public class App {
                 result = num1 - num2;
             } else if (str.equals("*")) {
                 result = num1 * num2;
-            } else {
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    flag2 = false;
-                } else {
+            } else if (str.equals("/")) {
+                if (num2 == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    System.out.println("flag2= "+flag2);
-                }
-            }
-        }
 
-        System.out.println("결과: "+result);
+                } else {
+                    result = num1 / num2;
+                }
+
+            } else {
+                System.out.println("오류: 정확한 연산자를 입력하세요");
+            }
+
+
+            System.out.println("결과: " + result);
+
+            System.out.println("계산기 사용을 멈추고 싶다면 'exit'을 정확하게 입력하세요.");
+            exit = scanner.next();
+        } while (!exit.equals("exit"));
     }
 }
+
